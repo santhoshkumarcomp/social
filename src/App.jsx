ebe0c101d5a3be9401513b6bef763fb5801e3eca
santhoshkumarcomp <santhoshkumarcomp@gmail.com>
2024-09-27 07:25:23 +0400

    getting started with react

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
       
     </>
